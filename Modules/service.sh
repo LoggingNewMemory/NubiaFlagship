b11b67999cdec12e2ad74225faf4baeb5bd6c229
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-08-08 21:52:37 +0700

    This also no need
    Signed-off-by: KanagawaYamada <albert.wesley.dion@gmail.com>

--- Modules/service.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-
-# Wait until boot is complete
-until [ "$(getprop sys.boot_completed)" = "1" ]; do
-    sleep 2
-done
-
-# I Hate to say only this works _- I have no choice but to rely on this
-sleep 15
