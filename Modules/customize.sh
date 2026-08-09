415674b2411fa34dea53b454324e5dee6a48b209
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-08-09 14:47:49 +0700

    Rebrand to Nubia Flagship
    Signed-off-by: KanagawaYamada <albert.wesley.dion@gmail.com>

--- Modules/customize.sh
+++ Modules/customize.sh
@@ -1,6 +1,6 @@
 LATESTARTSERVICE=true
 
-ui_print "- Nubia Neo 3 GT 5G Fixes -"
+ui_print "- Nubia Flagship -"
 ui_print "By: Kanagawa Yamada"
 ui_print "Device: $(getprop ro.product.model) ($(getprop ro.build.product))"
 ui_print "Android: $(getprop ro.build.version.release)"
